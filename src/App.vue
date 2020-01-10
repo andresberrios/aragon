42cbfb42c712d23292eb1890f5d0261c1dd740dd
andresberrios <andres.berrios.j@gmail.com>
2020-01-10 20:51:26 -0300

    Clean up

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1>Hotel</h1>
+    <Calendar />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Calendar from "./components/Calendar.vue";
 
 @Component({
   components: {
-    HelloWorld
+    Calendar
   }
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url("~bootstrap/dist/css/bootstrap.min.css");
 </style>
---
+++ src/components/Calendar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Calendar extends Vue {
+  // @Prop() private msg!: string;
+}
+</script>
+
+<style scoped lang="scss"></style>
