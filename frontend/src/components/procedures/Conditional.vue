ce3023cf457d41f4d6943376827c126d3b9f0e2b
andresberrios <andres.berrios.j@gmail.com>
2020-03-06 23:25:07 -0300

    Add support for else inside conditionals

--- frontend/src/components/procedures/Conditional.vue
+++ frontend/src/components/procedures/Conditional.vue
@@ -9,6 +9,18 @@
           required
           placeholder="Enter condition"
         />
+        <b-input-group-append>
+          <b-button
+            :variant="
+              step.else === undefined ? 'outline-info' : 'outline-danger'
+            "
+            @click="toggleElse(step)"
+          >
+            <b-icon icon="plus" v-if="step.else === undefined" />
+            <b-icon icon="dash" v-if="step.else !== undefined" />
+            ELSE
+          </b-button>
+        </b-input-group-append>
       </b-input-group>
       <b-form-text>
         Condition that should be met in order to proceed with the
@@ -18,6 +30,10 @@
     <div>
       <h3 class="h5">THEN</h3>
       <StepEditor :steps="step.then" :seq-prefix="`${seqId}.`" />
+    </div>
+    <div v-if="step.else !== undefined" class="mt-2">
+      <h3 class="h5">ELSE</h3>
+      <StepEditor :steps="step.else" :seq-prefix="`${seqId}.E.`" />
     </div>
   </div>
 </template>
@@ -35,5 +51,13 @@ export default class ConditionalComponent extends Vue {
 
   @Prop()
   seqId?: string;
+
+  toggleElse(step: Conditional) {
+    if (step.else === undefined) {
+      this.$set(step, "else", []);
+    } else {
+      step.else = undefined;
+    }
+  }
 }
 </script>
