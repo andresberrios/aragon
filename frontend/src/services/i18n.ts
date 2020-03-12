27c10992904d870d8ca1c66414f9ed2f6ed086e6
dannydebug <danica.v90@gmail.com>
2020-03-12 03:02:29 -0300

    Add spanish translation for datepicker

---
+++ frontend/src/services/i18n.ts
@@ -0,0 +1,14 @@
+export const datePickerLabels = {
+  labelPrevYear: "Año anterior",
+  labelPrevMonth: "Mes anterior",
+  labelCurrentMonth: "Mes actual",
+  labelNextMonth: "Próximo mes",
+  labelNextYear: "Próximo año",
+  labelToday: "Hoy",
+  labelSelected: "Fecha seleccionada",
+  labelNoDateSelected: "Sin fecha seleccionada",
+  labelCalendar: "Calendario",
+  labelNav: "Navegación del calendario",
+  labelHelp:
+    "Use las teclas del cursor para navegar por las fechas del calendario"
+};
